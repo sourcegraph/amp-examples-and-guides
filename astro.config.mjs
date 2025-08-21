1365faad8f651728d03a0a24a600575640e64e0c
Travis Lyons <travis.lyons@sourcegraph.com>
2025-08-21 14:25:31 -0400

    simplify to just use astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
   vite: {
     plugins: [tailwindcss()],
   },
